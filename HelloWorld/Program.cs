3debaa0e2e75ffac6d5a358fe11d7f44de161c37
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-04-19 14:36:57 +0530

    Created new Form and Updated Program run command to run it

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -16,7 +16,8 @@ namespace HelloWorld
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
